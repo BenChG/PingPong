8302ebad11aa65598eebf5a9ff6b57b000eb8ac6
Benedykt Chalubinski-Gonerko <ben.chalubinski@gmail.com>
2020-07-30 06:14:23 +0200

    Reload repository after adding .gitignore file

--- Unit1.cpp
+++ Unit1.cpp
@@ -4,6 +4,7 @@
 #pragma hdrstop
 
 #include "Unit1.h"
+#include "Winuser.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
@@ -46,3 +47,48 @@ void __fastcall TForm1::Timer_ballTimer(TObject *Sender)
 }
 //---------------------------------------------------------------------------
 
+
+void __fastcall TForm1::timer_left_bat_upTimer(TObject *Sender)
+{
+ left_bat->Top -= 10;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::timer_left_bat_downTimer(TObject *Sender)
+{
+  left_bat->Top += 10;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::timer_right_bat_upTimer(TObject *Sender)
+{
+  right_bat->Top -= 10;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::timer_right_bat_downTimer(TObject *Sender)
+{
+   right_bat->Top += 10;
+}
+//---------------------------------------------------------------------------
+
+
+void __fastcall TForm1::FormKeyDown(TObject *Sender, WORD &Key,
+      TShiftState Shift)
+{
+ if (Key == 'W') timer_left_bat_up->Enabled = true;
+ if (Key == 'S') timer_left_bat_down->Enabled = true;
+ if (Key == VK_UP) timer_right_bat_up->Enabled = true;
+ if (Key == VK_DOWN) timer_right_bat_down->Enabled = true;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::FormKeyUp(TObject *Sender, WORD &Key,
+      TShiftState Shift)
+{
+ if (Key == 'W') timer_left_bat_up->Enabled = false;
+ if (Key == 'S') timer_left_bat_down->Enabled = false;
+ if (Key == VK_UP) timer_right_bat_up->Enabled = false;
+ if (Key == VK_DOWN) timer_right_bat_down->Enabled = false;
+ }
+//---------------------------------------------------------------------------
--- Unit1.h
+++ Unit1.h
@@ -18,7 +18,19 @@ __published:	// IDE-managed Components
         TTimer *Timer_ball;
         TImage *left_bat;
         TImage *right_bat;
+        TTimer *timer_left_bat_up;
+        TTimer *timer_left_bat_down;
+        TTimer *timer_right_bat_up;
+        TTimer *timer_right_bat_down;
         void __fastcall Timer_ballTimer(TObject *Sender);
+        void __fastcall timer_left_bat_upTimer(TObject *Sender);
+        void __fastcall timer_left_bat_downTimer(TObject *Sender);
+        void __fastcall FormKeyDown(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall FormKeyUp(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall timer_right_bat_upTimer(TObject *Sender);
+        void __fastcall timer_right_bat_downTimer(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
         __fastcall TForm1(TComponent* Owner);
@@ -27,4 +39,3 @@ public:		// User declarations
 extern PACKAGE TForm1 *Form1;
 //---------------------------------------------------------------------------
 #endif
- 
